45123f63dab59aa5b99f2c78555a09b425077bc9
Ashley Collinge <ashleycollinge@aperture-trading.com>
2019-07-21 18:06:15 +0100

    added documentation to the flask mod_api

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,24 @@
+"""
+Test
+"""
 # Import flask and template operators
+import os
 from flask import Flask
 
 # Define the WSGI application object
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__, static_folder="static", static_url_path="/static")
 
 # Configurations
 app.config.from_object('config')
-
+"""
 # Sample HTTP error handling
 @app.errorhandler(404)
 def not_found(error):
     return "404"
+"""
 
 # Import a module / component using its blueprint handler variable (mod_auth)
 from app.mod_api.controllers import mod_api as api_module
 
 # Register blueprint(s)
 app.register_blueprint(api_module)
-# app.register_blueprint(xyz_module)
-# ..
--- app/mod_api/controllers.py
+++ app/mod_api/controllers.py
@@ -6,4 +6,29 @@ mod_api = Blueprint('api', __name__, url_prefix='/api')
 # Set the route and accepted methods
 @mod_api.route('/test/', methods=['GET'])
 def test():
-    return "hello world"
+    """
+    Returns test string.
+
+        **Example request**:
+
+        .. sourcecode:: http
+
+          GET /api/test1 HTTP/1.1
+          Accept: application/json
+
+        **Example response**:
+
+        .. sourcecode:: http
+
+          HTTP/1.1 200 OK
+          Content-Type: application/json
+
+          {"test-message": "Hello world."}
+
+        :query sort: No sorting.
+        :query q: N/A
+        :resheader Content-Type: application/json
+        :status 200: posts found
+        :returns: :class:`app.mod_api.StatusMessage`
+    """
+    return {"test-message": "Hello world."}
