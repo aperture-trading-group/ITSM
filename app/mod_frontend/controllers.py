abf94249e67c81f3042a0a1a6bd5dda2d1c8728b
Ashley Collinge <ashleycollinge@aperture-trading.com>
2019-07-21 21:45:16 +0100

    added documentation

--- app/mod_frontend/controllers.py
+++ app/mod_frontend/controllers.py
@@ -7,4 +7,24 @@ mod_frontend = Blueprint('frontend', __name__, url_prefix='/')
 @mod_frontend.route('/')
 @mod_frontend.route('/dashboard/', methods=['GET'])
 def dashboard():
+    """
+    Returns test string.
+
+        **Example request**:
+
+        .. sourcecode:: http
+
+          GET /api/test1 HTTP/1.1
+
+        **Example response**:
+
+        .. sourcecode:: http
+
+          HTTP/1.1 200 OK
+
+          mod_frontend:dashboard
+
+        :status 200:
+        :returns: mod_frontend:dashboard.html
+    """
     return render_template("mod_frontend/dashboard.html")
